b58321feb00d1c61a133a45943665bc48074b3ca
Paridhijain-githb <paridhijain13@gmail.com>
2025-11-07 22:29:11 +0530

    algorithm evaluation

---
+++ ALGORITHM EVALUATION/data_clean_preprocess.py
@@ -0,0 +1,29 @@
+# To clean the data and preprocess it
+ from sklearn.pipeline import make_pipeline
+ from sklearn.impute import SimpleImputer
+ from sklearn.preprocessing import StandardScaler,OneHotEncoder
+ from sklearn.compose import ColumnTransformer
+
+def clean_process_data(cat,con,X):
+    #creating numerical pipeline tat handles missing data and also scales numerical data 
+    num_pipe = make_pipeline(SimpleImputer(strategy='median'),
+    StandardScaler())
+    # creating categorial pipeline that handles missing data and scales text data 
+    cat_pipe = make_pipeline(SimpleImputer(strategy='most_frequent'),
+    OneHotEncoder(handle_unknown='ignore',sparse_output=False))
+    #combining both pipelines 
+    pre = ColumnTransformer([
+        ('cat',cat_pipe,cat),
+        ('con',num_pipe,con)
+    ]).set_output(transform = 'pandas')
+    #use the pre to fit and transform x data 
+    X_pre = pre.fit_transform(X)
+
+
+    #return the preprocessed X 
+    return X_pre
+
+
+    
+
+    
